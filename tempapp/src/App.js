736df7fa2d621fa3ca22b0f40d47dfe6f1272cf8
Santosh Balaaji S <sv.santosh2695@gmail.com>
2020-06-22 23:20:55 +1200

    initial commit

--- tempapp/src/App.js
+++ tempapp/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from '../src/Pages/Login'
+import RegistrationPage from '../src/Pages/Registration'
 
 function App() {
   return (
     <div className="App">
-     <LoginPage />
+     <RegistrationPage/>
     </div>
   );
 }
